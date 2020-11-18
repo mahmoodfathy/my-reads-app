1bcea95810515e007dbb53525d768a40dbc72751
mahmoodfathy <mahmoodfathy26@gmail.com>
2020-11-18 23:59:30 +0200

    Fix : fixed search bar bug

--- src/Components/Search.js
+++ src/Components/Search.js
@@ -1,20 +1,21 @@
-import React from 'react';
-import CloseSearchButton from './CloseSearchButton';
-import SearchInput from './SearchInput';
-import SearchResults from './SearchResults';
+import React from "react";
+import CloseSearchButton from "./CloseSearchButton";
+import SearchInput from "./SearchInput";
+import SearchResults from "./SearchResults";
 
-const Search = ({onSearch,searchedBooks,books,onShelfChange,onReset})=>{
-   
-    return (
-    <div className="search-books-bar">
+const Search = ({ onSearch, searchedBooks, books, onShelfChange, onReset }) => {
+  return (
+    <>
+      <div className="search-books-bar">
         <CloseSearchButton onReset={onReset} />
         <SearchInput onSearch={onSearch} />
-        <SearchResults
-          searchedBooks={searchedBooks}
-          books={books}
-          onShelfChange={onShelfChange}
-        />
-    </div>
-    );
-}
+      </div>
+      <SearchResults
+        searchedBooks={searchedBooks}
+        books={books}
+        onShelfChange={onShelfChange}
+      />
+    </>
+  );
+};
 export default Search;
